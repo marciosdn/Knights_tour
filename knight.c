aac5b6ac0789849c41c0eb3db7559f080c1057a5
Marcio <siqdn@yahoo.ca>
2020-01-27 11:04:36 -0500

    Update knight.c

--- knight.c
+++ knight.c
@@ -9,23 +9,23 @@
 
 int Sx[] = { 2, 1,-1,-2,-2,-1, 1, 2};
 int Sy[] = {-1,-2,-2,-1, 1, 2, 2, 1};
-int T[SIZEx][SIZEy];
+int T[SIZEx][SIZEy];    //Board x by y, zero filled.
 int step = 1;
-int sol = 1;
+int sol = 1;    //Solutions.
 
 void jump(int step, int x, int y) {
-	if (!T[x][y]) {
+	if (!T[x][y]) {    //If 0, it can walk.
 		T[x][y] = step++;
-        if (step>SIZEx*SIZEy) {    //is it finished?
-            printf("\nSolution: %d\n", sol++);
-            printT();    //print it!
-        } else {
-			for(int k=0;k<8;k++)
-                if (x+Sx[k]>-1 && y+Sy[k]>-1 && x+Sx[k]<SIZEx && y+Sy[k]<SIZEy)
-                    jump(step,x+Sx[k],y+Sy[k]);    //go!
+		if (step>SIZEx*SIZEy) {    //Is it finished, last step?
+		    printf("\nSolution: %d\n", sol++);
+		    printT();    //print it!
+		} else {
+			for(int k=0;k<8;k++)    //Try 8 directions.
+			if (x+Sx[k]>-1 && y+Sy[k]>-1 && x+Sx[k]<SIZEx && y+Sy[k]<SIZEy)
+			    jump(step,x+Sx[k],y+Sy[k]);    //go!
 		}
-        step--;
-        T[x][y] = 0;
+		step--;
+		T[x][y] = 0;
 	}
 }
 
